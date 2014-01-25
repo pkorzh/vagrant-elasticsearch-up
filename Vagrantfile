3ed61977f4e8de8e364900035c4dc4d5db989748
Platon Korzh <notalp@ya.ru>
2014-01-25 16:26:00 +0200

    x64 script fix

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,15 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
- 
-# Vagrant version 1.1.5
- 
-Vagrant.configure("2") do |config|
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
  
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.network :forwarded_port, guest: 9200, host: 9200
+  
+  config.vm.provider :virtualbox do |vbox|
+    vbox.customize ["modifyvm", :id, "--memory", 1048]
+  end
   
   config.vm.provision :shell, :path => "init.sh"
-  config.vm.network :forwarded_port, guest: 9200, host: 9200
  
 end
--- init.sh
+++ init.sh
@@ -1,28 +1,20 @@
-#!/usr/bin/env bash
- 
-apt-get -yq update
-apt-get -yq install git openjdk-7-jre-headless service-wrapper
- 
-# Install ElasticSearch
-wget -nv https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.6.tar.gz
-tar -xzf elasticsearch-0.20.6.tar.gz
-mv elasticsearch-0.20.6 /usr/local/elasticsearch
-rm elasticsearch-0.20.6.tar.gz
- 
-# Install the service
-mkdir /home/vagrant/repos
-git clone https://github.com/elasticsearch/elasticsearch-servicewrapper.git /home/vagrant/repos/elasticsearch-servicewrapper
-ln -s /home/vagrant/repos/elasticsearch-servicewrapper/service /usr/local/elasticsearch/bin/service
+#!/bin/bash
 
-# Install the ElasticSearch browser
-#/usr/local/elasticsearch/bin/plugin -q -install mobz/elasticsearch-head
-#/usr/local/elasticsearch/bin/plugin -q -install lukas-vlcek/bigdesk
+#Update apt-get +
+sudo apt-get update
  
-# Start ElasticSearch on boot
-/usr/local/elasticsearch/bin/service/elasticsearch install
+#Install Java +
+sudo apt-get install openjdk-7-jre-headless -y
 
-cp -u /vagrant/elasticsearch.yml /usr/local/elasticsearch/elasticsearch.yml
-cp -u /vagrant/logging.yml /usr/local/elasticsearch/logging.yml
- 
-# Start ElasticSearch now
-/usr/local/elasticsearch/bin/service/elasticsearch start
+#Download ElasticSearch .deb if changed
+wget -N https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.deb
+
+#Deploy ElasticSearch
+sudo dpkg -i --force-downgrade elasticsearch-0.90.10.deb
+
+#Update configuration
+sudo cp /vagrant/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /vagrant/logging.yml /etc/elasticsearch/logging.yml
+
+#Restart ElasticSearch
+sudo service elasticsearch restart
